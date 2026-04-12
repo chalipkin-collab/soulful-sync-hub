01f6a01debcc89e1400a9814edebfb1525de8776
chalipkin-collab <chalipkin@gmail.com>
2026-04-12 03:44:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+  base: '/soulful-sync-hub/',  server: {
     host: "::",
     port: 8080,
     hmr: {
